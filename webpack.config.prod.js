ceb4d625bd1f8f5db137748b753d1cdf2a6100d4
garethweaver <reg@garethweaver.com>
2019-09-10 12:13:34 +0100

    chore(add ES6 experimental class props)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -44,6 +44,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
